8d13866ba4f5fb4ee87d166101e7c20da110d980
Dru Serkes <DruSerkes@gmail.com>
2020-08-26 02:43:29 -0400

    add server

--- server/app.js
+++ server/app.js
@@ -9,10 +9,4 @@ app.use(express.json());
 // Some security
 app.use(helmet);
 
-// const app = require('./app');
-
-// app.listen(3000, () => {
-// 	console.log('App listening on port 3000');
-// });
-
 module.exports = app;
