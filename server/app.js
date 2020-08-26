ebaa490bef6fb0391b4609a0f95226c5c795e340
Dru Serkes <DruSerkes@gmail.com>
2020-08-26 02:50:37 -0400

    add error handlers

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const ExpressError = require('./helpers/ExpressError');
 
 const app = express();
 
@@ -12,5 +13,34 @@ app.use(helmet);
 
 // Request logger
 app.use(morgan('dev'));
+
+/*
+
+TODO 
+1. ADD ROUTE LOGIC FOR AUTHENTICATING A USER 
+2. ADD ROUTE LOGIC FOR SPOTIFY API
+
+*/
+
+/** 404 handler */
+
+app.use((req, res, next) => {
+	const err = new ExpressError('Not Found', 404);
+
+	// pass the error to the next piece of middleware
+	return next(err);
+});
+
+/** general error handler */
+
+app.use((err, req, res, next) => {
+	res.status(err.status || 500);
+	console.error(err.stack);
+
+	return res.json({
+		status  : err.status,
+		message : err.message
+	});
+});
 
 module.exports = app;
