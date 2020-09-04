370d9a4c2a2c756aa9ee736326e0dd258b770d01
Dru Serkes <DruSerkes@gmail.com>
2020-09-04 16:06:00 -0400

    update PORT environment variable

--- server/server.js
+++ server/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const { PORT } = require('./config');
 
 app.listen(PORT, () => {
-	console.log('App listening on port 3000');
+	console.log(`App listening on port ${PORT}`);
 });
