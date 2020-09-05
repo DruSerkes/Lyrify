b520704de2e258d9e1a8371dbdfba93c84652366
Dru Serkes <DruSerkes@gmail.com>
2020-09-05 02:18:56 -0400

    add cascades and remake DB

--- server/data.sql
+++ server/data.sql
@@ -1,25 +1,28 @@
-\c
-lyrify
+\c lyrify
 
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS songs;
-DROP TABLE IF EXISTS favorites;
+DROP TABLE IF EXISTS users
+CASCADE;
+DROP TABLE IF EXISTS songs
+CASCADE;
+DROP TABLE IF EXISTS favorites
+CASCADE;
+
 
 CREATE TABLE users
 (
-    id TEXT PRIMARY KEY
-    display_name TEXT PRIMARY KEY,
+    id TEXT PRIMARY KEY,
+    display_name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     product TEXT,
-    img_url URL,
+    img_url TEXT,
     accessToken TEXT,
-    refreshToken TEXT NOT NULL
+    refreshToken TEXT
 );
 
 
 CREATE TABLE songs
 (
-    id TEXT PRIMARY KEY,
+    id TEXT PRIMARY KEY ,
     artist TEXT NOT NULL,
     song TEXT NOT NULL,
     lyrics TEXT
@@ -27,6 +30,6 @@ CREATE TABLE songs
 
 CREATE TABLE favorites
 (
-    song_id TEXT REFERENCES songs(id),
-    user_id TEXT REFERENCES users(id)
+    song_id TEXT REFERENCES songs(id) ON DELETE CASCADE,
+    user_id TEXT REFERENCES users(id) ON DELETE CASCADE
 )
