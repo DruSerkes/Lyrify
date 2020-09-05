966650fdf205c0fc8a19fee431b2e51ddaf5edc6
Dru Serkes <DruSerkes@gmail.com>
2020-09-05 12:40:05 -0400

    User class - create method

--- server/models/models.js
+++ server/models/models.js
@@ -1,1 +1,15 @@
+const db = require('../db');
+const ExpressError = require('../helpers/ExpressError');
 
+class User {
+	static async create({ id, display_name, email, product, href, img_url, access_token, refresh_token }) {
+		const result = await db.query(
+			`INSERT INTO users (id, display_name, email, product, href, img_url, access_token, refresh_token)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
+			[ id, display_name, email, product, href, img_url, access_token, refresh_token ]
+		);
+
+		const user = result.rows[0];
+		return user;
+	}
+}
