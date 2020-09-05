1130ee6be07cc2905f1e112bb1a667bf5072d52b
Dru Serkes <DruSerkes@gmail.com>
2020-09-05 12:34:31 -0400

    add db connection

---
+++ server/db.js
@@ -0,0 +1,10 @@
+/** Setup database connection */
+
+const { Client } = require('pg');
+const { DB_URI } = require('./config');
+
+const db = new Client({ connectionString: DB_URI });
+
+db.connect();
+
+module.exports = db;
