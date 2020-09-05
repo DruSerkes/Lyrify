08870811b75e1ef5850ba8919b5d9b8fc97c5bc2
Dru Serkes <DruSerkes@gmail.com>
2020-09-05 12:41:59 -0400

    add docstring

--- server/models/models.js
+++ server/models/models.js
@@ -2,6 +2,12 @@ const db = require('../db');
 const ExpressError = require('../helpers/ExpressError');
 
 class User {
+	/** Create a user in the DB 
+     * 
+     * @param {*} spotifyUserData (obj)
+     * - container id, display_name, email, product, href, img_url, access_token, refresh_token
+     * @returns {user : {id, display_name, email, product, href, img_url, access_token, refresh_token}}
+     */
 	static async create({ id, display_name, email, product, href, img_url, access_token, refresh_token }) {
 		const result = await db.query(
 			`INSERT INTO users (id, display_name, email, product, href, img_url, access_token, refresh_token)
