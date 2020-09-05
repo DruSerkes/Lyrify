081f082933eefaa060d32d8dd443cd043658f9d9
Dru Serkes <DruSerkes@gmail.com>
2020-09-05 14:36:07 -0400

    add router. Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Routes from './Routes';
 
 function App() {
 	return (
 		<div className="App">
-			<p>App</p>
+			<Routes />
 		</div>
 	);
 }
