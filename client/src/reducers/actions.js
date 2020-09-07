44e4f69e6de7bd176dfef56150b268d0e60426b5
Dru Serkes <DruSerkes@gmail.com>
2020-09-07 16:25:28 -0400

    create actions for getUser, gotUser

--- client/src/reducers/actions.js
+++ client/src/reducers/actions.js
@@ -5,7 +5,14 @@ const BASE_URL = 'http://localhost:5000';
 
 export function getUser(id) {
 	return async function(dispatch) {
-        const res = await axios.get(`${BASE_URL}/users/${id}`);
-        
+		const res = await axios.get(`${BASE_URL}/users/${id}`);
+		dispatch(gotUser(res.data));
+	};
+}
+
+export function gotUser(userData) {
+	return {
+		type    : ADD_USER,
+		payload : userData
 	};
 }
