002fca8033f4d6dcc9b44d71672c741750a26bdd
Dru Serkes <DruSerkes@gmail.com>
2020-09-07 16:48:30 -0400

    import action type

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,8 +1,9 @@
+import { ADD_USER } from './actionTypes';
 const INITIAL_STATE = {};
 
 const userReducer = (state = INITIAL_STATE, action) => {
 	switch (action.type) {
-		case 'ADD_USER':
+		case ADD_USER:
 			return { ...state, user: action.payload };
 		default:
 			return state;
