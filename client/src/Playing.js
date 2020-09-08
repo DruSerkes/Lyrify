a82cd9e652c9bd52a6b708040ede4be2dad8a37e
Dru Serkes <DruSerkes@gmail.com>
2020-09-08 12:29:33 -0400

    btn click gets now playing

--- client/src/Playing.js
+++ client/src/Playing.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import { getNowPlaying } from './reducers/actions';
 
 const Playing = () => {
 	const dispatch = useDispatch();
-	const song = useSelector((state) => state.song);
+	const song = useSelector((state) => state.song.data);
+	const [ fetching, setFetching ] = useState(false);
+
+	const handleGetNowPlaying = () => {
+		setFetching(true);
+	};
+
+	useEffect(
+		() => {
+			if (fetching) {
+				dispatch(getNowPlaying());
+			}
+			return () => {
+				setFetching(false);
+			};
+		},
+		[ dispatch, fetching ]
+	);
+
+	return (
+		<Box padding={2} margin={2}>
+			<Typography variant="h1">Now Playing</Typography>
+			<Button onClick={handleGetNowPlaying}>Get Now Playing</Button>
+		</Box>
+	);
 };
