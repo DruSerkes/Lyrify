397b2caef9f0889f93efe85da1b8040da82ea30d
Dru Serkes <DruSerkes@gmail.com>
2020-09-11 15:34:47 -0400

    add logic to Search Component

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Typography } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-import {} from './reducers/actions';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+import { getSong } from './reducers/actions';
 import SearchForm from './SearchForm';
+import Lyrics from './Lyrics';
 
 const Search = () => {
 	const dispatch = useDispatch();
+	const songData = useSelector((state) => state.song.data, shallowEqual);
+	const [ searching, setSearching ] = useState(true);
 	const doSearch = (data) => {
-		dispatch();
+		dispatch(getSong(data));
+		setSearching(false);
 	};
 	return (
 		<Box>
 			<Typography variant="h1">Search Lyrics</Typography>
-			<SearchForm doSearch={doSearch} />
+			{searching ? <SearchForm doSearch={doSearch} /> : <Lyrics songData={songData} />}
+			{!searching ? (
+				<Button variant="contained" color="primary" onClick={handleGetNowPlaying}>
+					Search Again
+				</Button>
+			) : null}
 			{/* TODO create searchForm, render it, add logic here and pass it down */}
 			{/* on form submit, get the song data from spotify api, get lyrics, and render a Lyrics component with the songData passed down */}
 			{/* Reserve some state "searching?" that when changed (button click at the top/bottom of page) renders form again instead of lyrics  */}
