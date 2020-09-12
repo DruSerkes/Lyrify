7c9b0814d018808993cfbf83f4a6cbc35adaf6f8
Dru Serkes <DruSerkes@gmail.com>
2020-09-12 15:09:20 -0400

    create theme

---
+++ client/src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+	palette : {
+		primary   : {
+			main : green[500]
+		},
+		secondary : {
+			main : '#8C5612'
+		}
+	}
+});
+
+export default theme;
