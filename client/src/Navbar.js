a92977e1bb026d14cff654a6d83cc8b4d3e46c68
Dru Serkes <DruSerkes@gmail.com>
2020-09-13 02:21:42 -0500

    style Navbar update logo assets

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector, shallowEqual } from 'react-redux';
-import { AppBar, Avatar, Button, Toolbar } from '@material-ui/core';
+import { AppBar, Avatar, Button, Toolbar, Grid, Typography, Box, Divider } from '@material-ui/core';
 
 const Navbar = () => {
 	const user = useSelector((state) => state.user.data, shallowEqual);
 	return (
 		<AppBar position="sticky">
 			<Toolbar>
-				<Avatar>L</Avatar>
+				<Box display="flex" className="Navbar-Logo">
+					<Avatar src="logo500.png" alt="Lyrify" />
+					<Typography variant="body1"> yrify</Typography>
+				</Box>
 				{user !== undefined ? (
-					<div>
-						<Button color="inherit" component={NavLink} to="/playing">
-							Now Playing
-						</Button>
-						<Button color="inherit" component={NavLink} to="/search">
-							Search
-						</Button>
+					<React.Fragment>
+						<Grid container>
+							<Button
+								variant="text"
+								size="large"
+								className="Navbar-Btn"
+								color="inherit"
+								component={NavLink}
+								to="/playing"
+							>
+								Now Playing
+							</Button>
+							<Button
+								variant="text"
+								size="large"
+								className="Navbar-Btn"
+								color="inherit"
+								component={NavLink}
+								to="/search"
+							>
+								Search
+							</Button>
+						</Grid>
 						<Avatar sizes="lg" src={user.img_url} alt={user.display_name} />
-					</div>
+					</React.Fragment>
 				) : null}
 			</Toolbar>
 		</AppBar>
