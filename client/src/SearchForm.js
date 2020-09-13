9005c8bfbc5c4874315e2a07ac241a5562166509
Dru Serkes <DruSerkes@gmail.com>
2020-09-13 02:42:42 -0500

    add mui form control

--- client/src/SearchForm.js
+++ client/src/SearchForm.js
@@ -4,6 +4,7 @@ import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
 
 /**
  * Form Schema
@@ -24,32 +25,34 @@ const SearchForm = ({ doSearch }) => {
 
 	return (
 		<form className="SearchForm" onSubmit={handleSubmit(submit)}>
-			<TextField
-				inputRef={register}
-				error={errors.song}
-				name="song"
-				id="song"
-				label="Song Name"
-				variant="outlined"
-				margin="normal"
-				required
-				autoFocus
-			/>
-			<TextField
-				inputRef={register}
-				error={errors.artist}
-				name="artist"
-				id="artist"
-				label="Artist Name"
-				variant="outlined"
-				type=""
-				margin="normal"
-				required
-				autoFocus
-			/>
-			<Button onClick={handleSubmit(submit)} color="primary" variant="contained">
-				Search
-			</Button>
+			<FormControl>
+				<TextField
+					inputRef={register}
+					error={errors.song}
+					name="song"
+					id="song"
+					label="Song Name"
+					variant="outlined"
+					margin="normal"
+					required
+					autoFocus
+				/>
+				<TextField
+					inputRef={register}
+					error={errors.artist}
+					name="artist"
+					id="artist"
+					label="Artist Name"
+					variant="outlined"
+					type=""
+					margin="normal"
+					required
+					autoFocus
+				/>
+				<Button onClick={handleSubmit(submit)} color="primary" variant="contained">
+					Search
+				</Button>
+			</FormControl>
 		</form>
 	);
 };
