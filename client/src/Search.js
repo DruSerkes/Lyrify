23b109b45aef3cbe904a2e444fe79db7bf8125bd
Dru Serkes <DruSerkes@gmail.com>
2020-09-13 17:39:50 -0500

    remove unused components

--- client/src/Search.js
+++ client/src/Search.js
@@ -4,17 +4,12 @@ import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { getSong } from './reducers/actions';
 import SearchForm from './SearchForm';
 import Lyrics from './Lyrics';
-// import Button from '@material-ui/core/Button';
 
 const Search = () => {
 	const dispatch = useDispatch();
 	const songData = useSelector((state) => state.song.data, shallowEqual);
-	// const [ searching, setSearching ] = useState(true);
-	// const searchAgain = () => setSearching(true);
-	const doSearch = (data) => {
-		dispatch(getSong(data));
-		// setSearching(false);
-	};
+	const doSearch = (data) => dispatch(getSong(data));
+
 	return (
 		<Box>
 			<Typography variant="h1">Search Lyrics</Typography>
@@ -24,12 +19,5 @@ const Search = () => {
 		</Box>
 	);
 };
-/*
-{!searching ? (
-				<Button variant="contained" color="primary" onClick={searchAgain}>
-					Search Again
-				</Button>
-			) : null}
-			*/
 
 export default Search;
