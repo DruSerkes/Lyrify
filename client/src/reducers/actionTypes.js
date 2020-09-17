cb3b851ac31139bbfe2e3229fc4cd3a0855da983
Dru Serkes <DruSerkes@gmail.com>
2020-09-17 07:48:44 -0500

    add action types for add/remove favorite

--- client/src/reducers/actionTypes.js
+++ client/src/reducers/actionTypes.js
@@ -2,3 +2,5 @@ export const ADD_USER = 'ADD_USER';
 export const REMOVE_USER = 'REMOVE_USER';
 export const ADD_SONG = 'ADD_SONG';
 export const REMOVE_SONG = 'REMOVE_SONG';
+export const ADD_FAVORITE = 'ADD_FAVORITE';
+export const REMOVE_FAVORITE = 'REMOVE_FAVORITE';
