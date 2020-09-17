ec4ebd6299efc5a3ca2c6d9ec481a12497fd796a
Dru Serkes <DruSerkes@gmail.com>
2020-09-17 08:31:38 -0500

    update selector

--- client/src/Favorites.js
+++ client/src/Favorites.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
+import Favorite from './Favorite';
 
 const Favorites = () => {
 	// Select favorites
-	const favorites = useSelector((state) => state.user.favorites);
+	const user = useSelector((state) => state.user);
 	// render a list of "favorite" (dumb component) list items
 	return (
 		<React.Fragment>
 			<Typography>Favorites</Typography>
 			<List className="Favorites">
-				{favorites.length ? (
-					favorites.map((fav) => <Favorite artist={fav.artist} song={fav.song} id={fav.id} key={fav.id} />)
+				{user.favorites !== {} ? (
+					user.favorites.map((fav) => (
+						<Favorite artist={fav.artist} song={fav.song} id={fav.id} key={fav.id} />
+					))
 				) : (
 					<Typography>You haven't favorited any lyrics!</Typography>
 				)}
