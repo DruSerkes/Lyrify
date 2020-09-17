b28beb7e9c32debfe7f61917fa3f0b338b0b999b
Dru Serkes <DruSerkes@gmail.com>
2020-09-17 07:36:08 -0500

    remove unused import

--- client/src/Favorites.js
+++ client/src/Favorites.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import Typography from '@material-ui/core/Typography';
 
 const Favorites = () => {
