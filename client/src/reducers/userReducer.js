6bd91dabc00db04c818cc5357cfa4cc442e9ee93
Dru Serkes <DruSerkes@gmail.com>
2020-09-17 08:15:25 -0500

    add cases for favoriting to userReducer

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,16 +1,22 @@
-import { ADD_USER, REMOVE_USER } from './actionTypes';
+import { ADD_FAVORITE, ADD_USER, REMOVE_USER, REMOVE_FAVORITE } from './actionTypes';
 const INITIAL_STATE = {};
 
 const userReducer = (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case ADD_USER:
-			localStorage.setItem('id', action.payload.id);
-			return { ...state, data: action.payload };
+			// localStorage.setItem('id', action.payload.id);
+			return { ...state, ...action.payload };
 		case REMOVE_USER:
-			localStorage.removeItem('id');
-			return { ...state, data: {} };
-		// TODO add case ADD_FAVORITE
-		// TODO add case REMOVE_FAVORITE
+			// localStorage.removeItem('id');
+			return {};
+		case ADD_FAVORITE:
+			let idToAdd = action.payload.id;
+			return { ...state, favorites: { ...state.favorites, [idToAdd]: action.payload } };
+		case REMOVE_FAVORITE:
+			const idToRemove = action.id;
+			const favoritesCopy = state.favorites;
+			delete favoritesCopy[idToRemove];
+			return { ...state, favorites: { ...favoritesCopy } };
 		default:
 			return state;
 	}
