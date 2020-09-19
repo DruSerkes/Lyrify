287d5dcb88280ee3be2618b16247844486fc0576
Dru Serkes <DruSerkes@gmail.com>
2020-09-19 00:14:32 -0500

    remove commented out code

--- client/src/Favorites.js
+++ client/src/Favorites.js
@@ -1,21 +1,9 @@
 import React from 'react';
-// import { useDispatch, shallowEqual } from 'react-redux';
-// import { getFavorites } from './reducers/actions';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Favorite from './Favorite';
 
 const Favorites = ({ user, favorites }) => {
-	// const dispatch = useDispatch();
-	// // const favorites = useSelector((state) => state.favorites, shallowEqual);
-
-	// useEffect(
-	// 	() => {
-	// 		if (!favorites.length) dispatch(getFavorites(user.id));
-	// 	},
-	// 	[ favorites, dispatch, user.id ]
-	// );
-
 	return (
 		<React.Fragment>
 			<Typography variant="h1">Favorites</Typography>
