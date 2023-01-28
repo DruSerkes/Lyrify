79416c51751daf141d91eac163e597498b3f39dd
dru serkes <druserkes@gmail.com>
2023-01-28 11:30:40 -0500

    Add server logs

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ app.use(morgan('dev'));
 
 // Root redirects to the root for the front end
 app.get('/', (req, res, next) => {
-	return res.redirect(HOME);
+  return res.redirect(HOME);
 });
 
 // User Routes
@@ -38,93 +38,100 @@ app.use('/users', userRoutes);
 
 // Search for lyrics by user input
 app.post('/search', async (req, res, next) => {
-	try {
-		const songData = req.body;
-		const result = jsonschema.validate(songData, searchSongSchema);
-		if (!result.valid) throw new ExpressError('invalid song data', 400);
-		const song = await fetchLyrics(songData);
-		song.id = uuidv4();
-		return res.json({ song });
-	} catch (e) {
-		return next(e);
-	}
+  try {
+    const songData = req.body;
+    const result = jsonschema.validate(songData, searchSongSchema);
+    if (!result.valid) throw new ExpressError('invalid song data', 400);
+    const song = await fetchLyrics(songData);
+    song.id = uuidv4();
+    return res.json({ song });
+  } catch (e) {
+    return next(e);
+  }
 });
 
 // Get a song
 app.get('/songs/:id', async (req, res, next) => {
-	try {
-		const { id } = req.params;
-		const song = await Song.getById(id);
-		return res.json({ song });
-	} catch (e) {
-		return next(e);
-	}
+  try {
+    const { id } = req.params;
+    const song = await Song.getById(id);
+    return res.json({ song });
+  } catch (e) {
+    return next(e);
+  }
 });
 
 // Spotify auth for login
 app.get('/spotify/auth', (req, res, next) => {
-	try {
-		const spotifyApi = new SpotifyWebApi({ redirectUri, clientId, clientSecret, state });
-		const authUrl = spotifyApi.createAuthorizeURL(scopes, state);
-		res.redirect(authUrl);
-	} catch (e) {
-		return next(e);
-	}
+  try {
+    const spotifyApi = new SpotifyWebApi({ redirectUri, clientId, clientSecret, state });
+    console.log({ spotifyApi })
+    const authUrl = spotifyApi.createAuthorizeURL(scopes, state);
+    console.log({ authUrl })
+    res.redirect(authUrl);
+  } catch (e) {
+    console.log(e);
+    return next(e);
+  }
 });
 
 // Spotify redirect uri for login
 app.get('/callback', async (req, res, next) => {
-	try {
-		const spotifyApi = new SpotifyWebApi({ redirectUri, clientId, clientSecret, state });
-		const { code } = req.query;
-		const authData = await spotifyApi.authorizationCodeGrant(code);
-		const { access_token, refresh_token } = authData.body;
-		spotifyApi.setAccessToken(access_token);
-		spotifyApi.setRefreshToken(refresh_token);
+  try {
+    const spotifyApi = new SpotifyWebApi({ redirectUri, clientId, clientSecret, state });
+    console.log({ spotifyApi });
+    const { code } = req.query;
+    const authData = await spotifyApi.authorizationCodeGrant(code);
+    console.log({ authData })
+    const { access_token, refresh_token } = authData.body;
+    spotifyApi.setAccessToken(access_token);
+    spotifyApi.setRefreshToken(refresh_token);
 
-		// Get user data from Spotify
-		const me = await spotifyApi.getMe();
-		const { id, display_name, email, href, product } = me.body;
-		// const img_url = me.body.images[0].url || null;
-		const img_url = getImgUrl(me);
-		// Check if user already in DB
-		const dbUser = await User.getById(id);
-		if (dbUser) {
-			const updatedTokens = await User.updateTokens(id, access_token, refresh_token);
-			delete dbUser.access_token;
-			delete dbUser.refresh_token;
-			return res.redirect(`${HOME}?${querystring.stringify(dbUser)}`);
-		} else {
-			const userData = { id, display_name, email, href, product, img_url, access_token, refresh_token };
-			const newUser = await User.create(userData);
-			delete newUser.refresh_token;
-			return res.redirect(`${HOME}?${querystring.stringify(newUser)}`);
-		}
-	} catch (e) {
-		console.log('something went wrong', e.message);
-		return next(e);
-	}
+    // Get user data from Spotify
+    const me = await spotifyApi.getMe();
+    console.log({ me });
+    const { id, display_name, email, href, product } = me.body;
+    // const img_url = me.body.images[0].url || null;
+    const img_url = getImgUrl(me);
+    // Check if user already in DB
+    const dbUser = await User.getById(id);
+    console.log({ dbUser });
+    if (dbUser) {
+      const updatedTokens = await User.updateTokens(id, access_token, refresh_token);
+      delete dbUser.access_token;
+      delete dbUser.refresh_token;
+      return res.redirect(`${HOME}?${querystring.stringify(dbUser)}`);
+    } else {
+      const userData = { id, display_name, email, href, product, img_url, access_token, refresh_token };
+      const newUser = await User.create(userData);
+      delete newUser.refresh_token;
+      return res.redirect(`${HOME}?${querystring.stringify(newUser)}`);
+    }
+  } catch (e) {
+    console.log('something went wrong', e.message);
+    return next(e);
+  }
 });
 
 /** 404 handler */
 
 app.use((req, res, next) => {
-	const err = new ExpressError('Not Found', 404);
+  const err = new ExpressError('Not Found', 404);
 
-	// pass the error to the next piece of middleware
-	return next(err);
+  // pass the error to the next piece of middleware
+  return next(err);
 });
 
 /** general error handler */
 
 app.use((err, req, res, next) => {
-	res.status(err.status || 500);
-	console.error(err.stack);
+  res.status(err.status || 500);
+  console.error(err.stack);
 
-	return res.json({
-		status  : err.status,
-		message : err.message
-	});
+  return res.json({
+    status: err.status,
+    message: err.message
+  });
 });
 
 module.exports = app;
--- server/config.js
+++ server/config.js
@@ -8,7 +8,7 @@ const BCRYPT_WORK_FACTOR = +process.env.BCRYPT_WORK_FACTOR || 14;
 const clientId = process.env.clientId;
 const clientSecret = process.env.clientSecret;
 const redirectUri = process.env.redirectUri || 'http://localhost:5000/callback/';
-const scopes = [ 'user-read-private', 'user-read-email', 'user-read-currently-playing' ];
+const scopes = ['user-read-private', 'user-read-email', 'user-read-currently-playing'];
 const HOME = process.env.HOMEPAGE_URL || 'http://localhost:3000/';
 const state = process.env.state || 'some state of my choice';
 
@@ -18,23 +18,17 @@ const state = process.env.state || 'some state of my choice';
 // - in testing, 'lyrify-test'
 // - else: 'lyrify'
 
-let DB_URI;
-
-if (process.env.NODE_ENV === 'test') {
-	DB_URI = 'lyrify-test';
-} else {
-	DB_URI = process.env.DATABASE_URL || 'lyrify';
-}
+const DB_URI = process.env.NODE_ENV === 'test' ? 'lyrify-test' : process.env.DATABASE_URL || 'lyrify'
 
 module.exports = {
-	SECRET_KEY,
-	PORT,
-	DB_URI,
-	BCRYPT_WORK_FACTOR,
-	clientId,
-	clientSecret,
-	redirectUri,
-	scopes,
-	HOME,
-	state
+  SECRET_KEY,
+  PORT,
+  DB_URI,
+  BCRYPT_WORK_FACTOR,
+  clientId,
+  clientSecret,
+  redirectUri,
+  scopes,
+  HOME,
+  state
 };
